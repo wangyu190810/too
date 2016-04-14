780f60a950e6374cbe3c7058bb35e1abef220632
22too <22too>
2016-04-15 07:43:50 +0800

    finish loop read request buf and return request, but this code look very ugly.

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,40 @@ fn post(form_arges: &str) -> String{
     return response
 }
 
-fn request(method: HashMap, arges: Vec<&str>){
-    if method.contains_key("GET"){
-        
-        return "GET Method"
-    }
-    else if method.contains_key("POST"){
-        
-        return "POST Method"
-    }
-    else{
-    
-        return "Other Method"
-    }
-
-}
-
-fn roule(url : &str,method: &str){
-
-    let mut methods = HashMap::new();
-    let method: HashMap = match method {
-        "GET" => methods.insert("GET",)
-        "POST" => post(method[1]),
-         _ => "other method".to_string()
-    };
-    request(url,)
-    
-
-}
-
-
+//fn request(method: HashMap, arges: Vec<&str>){
+//    if method.contains_key("GET"){
+//        
+//        return "GET Method"
+//    }
+//    else if method.contains_key("POST"){
+//        
+//        return "POST Method"
+//    }
+//    else{
+//    
+//        return "Other Method"
+//    }
+//
+//}
+//
+//fn roule(url : &str,method: &str){
+//
+//    let mut methods = HashMap::new();
+//    let method: HashMap = match method {
+//        "GET" => methods.insert("GET"),
+//        "POST" => post(method[1]),
+//         _ => "other method".to_string()
+//    };
+//    request(url,)
+//    
+//
+//}
+//
+//
 
 fn response(res: &str) -> String{
     
-    
-
+    "Response".to_string()
     
 }
 
@@ -77,22 +76,60 @@ fn header(read_data: &str) -> String{
     let method : Vec<&str> = client_data[0].split(' ').collect();
     println!("{:?}",method[0]);
 
-    //let mut response = match method[1] {
-    //    "GET" => get(method[1],get_callback),
-    //    "POST" => post(method[1]),
-    //     _ => "other method".to_string()
-    //};
+    let mut response = match method[1] {
+        "GET" => get(method[1],get_callback),
+        "POST" => post(method[1]),
+         _ => "other method".to_string()
+    };
     return response
 }
 
 
 
 fn handler(mut stream : TcpStream){
-    let mut read_data = [0;1024];
-    stream.read(&mut read_data);
-    let mut response = header(str::from_utf8(&read_data).unwrap());
-    stream.write(response.as_bytes());
+    //let mut request = vec![""];
+    //let mut buf = [0;1024];
+    let mut buf = [0;128];
+    //request.push(str::from_utf8(&buf).unwrap());
 
+    let mut requests:Vec<u8> = Vec::new();
+    let mut request:u8;
+    let mut read_data = [0;128];
+    //let mut read_data = [u8];
+    loop{
+        match stream.read(&mut buf) {
+            Ok(count) => {
+                if (count == 128) || (count > 0){
+                    println!("message len: {}",count);
+                    read_data = buf;
+                    let mut flag = 0;
+                    loop{
+                    
+                        &requests.push(buf[flag]);
+                        flag += 1;
+                        if flag == count - 1{
+                            break;
+                        }
+                    }
+                    if count < 128{
+                        break;
+                    }
+                }
+                else {
+                    println!("message len: {}",count);
+                    break;
+                }
+            }
+             _ => {
+                break;
+            }
+
+        }
+    
+    println!("message end data: {:}",str::from_utf8(&requests).unwrap());
+    }
+    let mut response = str::from_utf8(&requests).unwrap().to_string();
+    stream.write(response.as_bytes());
 }
 
 fn server(){
